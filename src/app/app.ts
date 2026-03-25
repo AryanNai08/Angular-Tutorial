964fc71086bd292ab18234396414acc76892891f
Nai Aryan <130205139+AryanNai08@users.noreply.github.com>
2026-03-25 15:25:38 +0530

    (Angular):-Interpolation

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,4 +9,9 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('angular-app');
+  protected readonly name = signal('Aryan Nai');  
+  helloworld(){
+    let x=20;
+    console.log(x);
+  }
 }
