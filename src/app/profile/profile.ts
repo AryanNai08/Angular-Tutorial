2e02103b11e18985ede665623c4f3f2747d71b34
Nai Aryan <130205139+AryanNai08@users.noreply.github.com>
2026-03-25 16:00:24 +0530

    (Angular):-function call on button click

--- src/app/profile/profile.ts
+++ src/app/profile/profile.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { TitleStrategy } from "@angular/router";
 @Component({
         selector:'app-profile',
         templateUrl:'./profile.html',
@@ -6,4 +7,12 @@ import { Component } from "@angular/core";
     })
 export class ProfileComponenet {
  
+    HandleClick(){
+        alert("Button Clicked");
+        console.log("Button Clicked");
+        this.otherfun();    
+    }
+    otherfun(){
+        console.log("Other function called");
+    }
 }
